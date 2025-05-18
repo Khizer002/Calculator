4fa661cf0b6c0dcf7b3a5157090d74d34500e35a
Khizer002 <khizerahmed002@gmail.com>
2025-05-18 18:36:28 +0500

    A Physical looking Calculator

---
+++ app1.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("calculator.html")
+
+
+@app.route("/result", methods=["POST"])
+def result():
+    expression = request.form["expression"]
+    operators = ["+", "-", "*", "/"]
+
+    for op in operators:
+        if op in expression:
+            exp = expression.split(op)
+            if exp[0].isdigit() and exp[1].isdigit():
+                num1 = float(exp[0])
+                num2 = float(exp[1])
+                if (op=='+'):
+                    res=num1 + num2
+                elif (op=='-'):
+                    res=num1 - num2
+                elif (op=='*'):
+                    res=num1 * num2
+                elif (op=='/'):
+                    res = num1 / num2
+                return render_template("calculator.html",res=res)
+            else:
+                return "<h1>Invalid Expression</h1>"
+
+    return "<h1>Wrong input</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
